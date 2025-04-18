13d37f396a7ed6d8e824406e2f518919c05581e3
Saqlain <riyanahmed1703@gmail.com>
2025-04-18 15:51:57 +0530

    Sender and receiver connections are established

--- frontend/src/Components/receiver.tsx
+++ frontend/src/Components/receiver.tsx
@@ -1,16 +1,34 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from "react";
 
 function Receiver() {
-      useEffect(() => {
-          const socket = new WebSocket('ws://localhost:8080/ws')
-          socket.onopen = () =>{
-              console.log('WebSocket connection established')
-              socket.send(JSON.stringify({ type: 'receiver' }))
-          }
-      },[])
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080/ws");
+    setSocket(socket);
+    socket.onopen = () => {
+      console.log("WebSocket connection established");
+      socket.send(JSON.stringify({ type: "receiver" }));
+    };
+  }, []);
+  if (socket) {
+    socket.onmessage = async (event) => {
+      const message = JSON.parse(event.data);
+      if (message.type === "createOffer") {
+        const pc = new RTCPeerConnection();
+        await pc.setRemoteDescription(message.sdp);
+        const answer = await pc.createAnswer();
+        await pc.setLocalDescription(answer);
+        socket.send(
+          JSON.stringify({ type: "createAnswer", sdp: pc.localDescription })
+        );
+      }
+    };
+  }
   return (
-    <div>Receiver</div>
-  )
+    <>
+      <div>Receiver</div>
+    </>
+  );
 }
 
-export default Receiver
+export default Receiver;
--- frontend/src/Components/sender.tsx
+++ frontend/src/Components/sender.tsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
 function Sender() {
-    useEffect(() => {
-        const socket = new WebSocket('ws://localhost:8080/ws')
-        socket.onopen = () =>{
-            console.log('WebSocket connection established')
-            socket.send(JSON.stringify({ type: 'sender' }))
-        }
-    },[])
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080/ws");
+    setSocket(socket);
+    socket.onopen = () => {
+      console.log("WebSocket connection established");
+      socket.send(JSON.stringify({ type: "sender" }));
+    };
+  }, []);
+  const handleSend = async () => {
+    if (!socket) return;
+    const pc = new RTCPeerConnection();
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+    socket?.send(
+      JSON.stringify({ type: "createOffer", sdp: pc.localDescription })
+    );
+    socket.onmessage = async(msg) =>{
+      const message = JSON.parse(msg.data);
+      if(message.type==="createAnswer"){
+        await pc.setRemoteDescription(message.sdp);
+      }
+    }
+  };
+
   return (
-    <div>Sender</div>
-  )
+    <>
+      <div>Sender</div>
+      <button onClick={handleSend}>Send</button>
+    </>
+  );
 }
 
-export default Sender
+export default Sender;
